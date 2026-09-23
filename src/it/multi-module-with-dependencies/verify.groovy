8721c05a4f2b110a38a8899e818517ef4b2bc500
Guillaume Nodet <guillaume.nodet@gmail.com>
2026-09-23 11:54:54 +0000

    Add verify.groovy and fix IT conventions for MCLEAN-104
    - Add verify.groovy to assert clean succeeds without dependency resolution
    - Rename groupId/artifactId to follow IT conventions (groupId=test)
    - Drop redundant <url> tag from root pom
    - Use @pom.version@ token (consistent with other ITs in this project)
    - Drop redundant compiler properties from child modules (inherited from parent)

---
+++ src/it/multi-module-with-dependencies/verify.groovy
@@ -0,0 +1,38 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Verify that clean succeeds in a multi-module project where one module depends on
+// another without attempting dependency resolution (MCLEAN-104).
+// The clean lifecycle should not resolve dependencies, so module1 need not be installed.
+
+def log = new File( basedir, 'build.log' ).text
+
+if ( !log.contains( 'BUILD SUCCESS' ) )
+{
+    System.out.println( 'FAILURE: expected BUILD SUCCESS' )
+    return false
+}
+
+if ( log.contains( 'Could not resolve dependencies' ) || log.contains( 'DependencyResolutionException' ) )
+{
+    System.out.println( 'FAILURE: clean should not attempt dependency resolution' )
+    return false
+}
+
+return true
